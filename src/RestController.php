58dc6197d383b419406cf4f39683f26251298d43
Alif Bintoro <alifbintoro77@gmail.com>
2020-09-09 10:59:33 +0700

    Remove HTTP Status Code

--- src/RestController.php
+++ src/RestController.php
@@ -233,14 +233,6 @@ class RestController extends \CI_Controller
     private $format;
 
     /**
-     * @var Http Status Code
-     */
-    private $http_status = [
-        'METHOD_NOT_ALLOWED' => 405,
-        'UNAUTHORIZED' => 401
-    ];
-
-    /**
      * @var bool
      */
     protected $auth_override;
